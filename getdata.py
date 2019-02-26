1ef31d488843acb66ef67c51dfb9b7cb70f72539
fyr233 <36994804+fyr233@users.noreply.github.com>
2019-02-26 23:00:04 +0800

    add netspeed
    加入网速显示

--- getdata.py
+++ getdata.py
@@ -1,13 +1,16 @@
 import requests
 import re
+import time
+import subprocess
 
-teach_url = 'https://www.teach.ustc.edu.cn/'
+netspeed_log = {'time':[],
+                'speed':[],
+                'ping':{'baidu.com':[],
+                        'ustc.edu.cn':[]},
+                }
 
-def getdata():
-    f0 = open('webpage/index.html', 'r', encoding='UTF-8')
-    newhtml = f0.read()
-    f0.close()
-    f = open('webpage/index.html', 'w', encoding='UTF-8')
+def get_teach_ustc_edu_cn(newhtml):
+    teach_url = 'https://www.teach.ustc.edu.cn/'
     #更新教务处通知
     print('正在更新教务处通知')
     r = requests.get(teach_url)
@@ -17,9 +20,41 @@ def getdata():
             news_content = news_content[0]
             newhtml = re.sub('<section class="notice-fp">[\s\S]*?</section>', news_content, newhtml)
             newhtml = re.sub('<section class="notice-fp">[\s\S]*?<header>[\s\S]*?</header>', '<section class="notice-fp">\n\t<header>\n<h2><a href="https://www.teach.ustc.edu.cn/category/notice">教务处通知</a></h2>\n\t<div style="height:3px;width:100%;display: block;background:#EEEEEE ;clear: both;"></div>\n</header>', newhtml)
-            
     else:
         print('网络错误：', r.status_code)
+    return newhtml
+
+def get_test_ustc_edu_cn():
+    #测试带宽
+    url = 'http://test.ustc.edu.cn/'
+    r = requests.get(url)
+    speed = len(r.content)/r.elapsed.total_seconds()*8/1024/1024
+    r = requests.get(url)
+    speed = (speed + len(r.content)/r.elapsed.total_seconds()*8/1024/1024)/2
+    #测试ping
+    sites = ['baidu.com','ustc.edu.cn']
+    for each in sites:
+        p = subprocess.Popen('ping '+each, shell = True, stdout = subprocess.PIPE)
+        p.wait()
+        #print(p.stdout.read().decode('gbk'))
+        netspeed_log['ping'][each].append( int(re.findall('平均 = (.*?)ms', p.stdout.read().decode('gbk'))[0]))
+
+    netspeed_log['time'].append(time.strftime("%H:%M", time.localtime()))
+    netspeed_log['speed'].append(speed)
+
+    if len(netspeed_log)>300:
+        netspeed_log.pop(0)
+    with open('webpage/static/js/netspeedandping.js', 'w', encoding='UTF-8') as dataf:
+        dataf.write('var netspeeddata = '+str(netspeed_log)+';')
+
+def getdata():
+    f0 = open('webpage/index.html', 'r', encoding='UTF-8')
+    newhtml = f0.read()
+    f0.close()
+    f = open('webpage/index.html', 'w', encoding='UTF-8')
+    newhtml = get_teach_ustc_edu_cn(newhtml)
+    get_test_ustc_edu_cn()
+    #print(newhtml)
     f.write(newhtml)
     f.close()
     
--- webpage/static/js/draw.js
+++ webpage/static/js/draw.js
@@ -1,6 +1,7 @@
 // 基于准备好的dom，初始化echarts实例
 var Chart_test1 = echarts.init(document.getElementById('test1'), 'mydark');
 var Chart_showtime = echarts.init(document.getElementById('clock'), 'mydark');
+var Chart_netspeed = echarts.init(document.getElementById('netspeed'), 'mydark');
 
 // 指定图表的配置项和数据
 
@@ -41,7 +42,104 @@ Chart_showtime.setOption(option = {
         },
         left: 'center',
         top: "center",
+        padding: 15,
         text: date.toLocaleTimeString().slice(0,-3),
         borderRadius: [5, 5, 0, 0]
     },
 });
+
+Chart_netspeed.setOption(option = {
+    title : {
+        text: '网\n速',
+        textStyle:{
+            fontSize: 60,
+            fontFamily: "'Microsoft YaHei', '微软雅黑', Arial, Verdana, 'sans-serif'",
+        },
+        padding: 20
+    },
+    tooltip : {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['网速', '', 'ping-baidu.com', '', 'ping-ustc.edu.cn'],
+        textStyle:{
+            fontSize: 25,
+            fontFamily: "'Microsoft YaHei', '微软雅黑', Arial, Verdana, 'sans-serif'",
+        },
+        padding: 10
+    },
+    calculable : true,
+    grid:{
+        //x:100,
+        y:140,
+        width:1100,
+        height:400,
+    },
+    xAxis : [
+        {
+            type : 'category',
+            // x轴的字体样式
+            axisLabel: {        
+                show: true,
+                textStyle: {
+                    color: '#EEEEEE',
+                    fontSize:16
+                }
+            },
+            boundaryGap : false,
+            data : netspeeddata['time']
+        }
+    ],
+    yAxis : [
+        {
+            name: 'Mbps',
+            type: 'value',
+            axisLabel: {        
+                show: true,
+                textStyle: {
+                    color: '#EEEEEE',
+                    fontSize:16
+                }
+            },
+        },
+        {
+            name: 'ms',
+            type: 'value',
+            axisLabel: {        
+                show: true,
+                textStyle: {
+                    color: '#EEEEEE',
+                    fontSize:16
+                }
+            },
+        }
+    ],
+    animation: false,
+    series : [
+        {
+            name:'网速',
+            type:'line',
+            yAxisIndex: 0,
+            smooth:true,
+            itemStyle: {normal: {areaStyle: {type: 'default'}}},
+            data: netspeeddata['speed']
+        },
+        {
+            name:'ping-baidu.com',
+            type:'line',
+            yAxisIndex: 1,
+            smooth:true,
+            itemStyle: {normal: {areaStyle: {type: 'default'}}},
+            data: netspeeddata['ping']['baidu.com']
+        },
+        {
+            name:'ping-ustc.edu.cn',
+            type:'line',
+            yAxisIndex: 1,
+            smooth:true,
+            itemStyle: {normal: {areaStyle: {type: 'default'}}},
+            data: netspeeddata['ping']['ustc.edu.cn']
+        }
+    ]
+});
+                    
